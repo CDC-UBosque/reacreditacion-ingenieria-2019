c298e24ac631481978c948fef6a3a2bce80d9cd4
Unknown <seguragabriel@unbosque.edu.co>
2019-10-22 16:54:25 -0500

    wip oct 22

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,107 @@
 (function ($) {
 	
-	$(".image-neurona").click(function() {
-		$(".content-neurona").toggleClass("open");
-		$(".image-neurona").toggleClass("move");
+	// -------------
+	// Neurona 1
+	
+	$(".neurona-1 .image-neurona").click(function() {
+		$(".neurona-1 .content-neurona").toggleClass("open");
+		$(".neurona-1 .image-neurona").toggleClass("move");
 	});
 	
-	$(".close-button").click(function() {
-		$(".content-neurona").toggleClass("open");
-		$(".image-neurona").toggleClass("move");
+	$(".neurona-1 .close-button").click(function() {
+		$(".neurona-1 .content-neurona").toggleClass("open");
+		$(".neurona-1 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 2
+	
+	$(".neurona-2 .image-neurona").click(function() {
+		$(".neurona-2 .content-neurona").toggleClass("open");
+		$(".neurona-2 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-2 .close-button").click(function() {
+		$(".neurona-2 .content-neurona").toggleClass("open");
+		$(".neurona-2 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 3
+	
+	$(".neurona-3 .image-neurona").click(function() {
+		$(".neurona-3 .content-neurona").toggleClass("open");
+		$(".neurona-3 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-3 .close-button").click(function() {
+		$(".neurona-3 .content-neurona").toggleClass("open");
+		$(".neurona-3 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 4
+	
+	$(".neurona-4 .image-neurona").click(function() {
+		$(".neurona-4 .content-neurona").toggleClass("open");
+		$(".neurona-4 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-4 .close-button").click(function() {
+		$(".neurona-4 .content-neurona").toggleClass("open");
+		$(".neurona-4 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 5
+	
+	$(".neurona-5 .image-neurona").click(function() {
+		$(".neurona-5 .content-neurona").toggleClass("open");
+		$(".neurona-5 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-5 .close-button").click(function() {
+		$(".neurona-5 .content-neurona").toggleClass("open");
+		$(".neurona-5 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 6
+	
+	$(".neurona-6 .image-neurona").click(function() {
+		$(".neurona-6 .content-neurona").toggleClass("open");
+		$(".neurona-6 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-6 .close-button").click(function() {
+		$(".neurona-6 .content-neurona").toggleClass("open");
+		$(".neurona-6 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 7
+	
+	$(".neurona-7 .image-neurona").click(function() {
+		$(".neurona-7 .content-neurona").toggleClass("open");
+		$(".neurona-7 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-7 .close-button").click(function() {
+		$(".neurona-7 .content-neurona").toggleClass("open");
+		$(".neurona-7 .image-neurona").toggleClass("move");
+	});
+	
+	// -------------
+	// Neurona 8
+	
+	$(".neurona-8 .image-neurona").click(function() {
+		$(".neurona-8 .content-neurona").toggleClass("open");
+		$(".neurona-8 .image-neurona").toggleClass("move");
+	});
+	
+	$(".neurona-8 .close-button").click(function() {
+		$(".neurona-8 .content-neurona").toggleClass("open");
+		$(".neurona-8 .image-neurona").toggleClass("move");
 	});
 	
 })(jQuery);
